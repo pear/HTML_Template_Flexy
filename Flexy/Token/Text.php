3026b2546c7a3f7aa5fa65941653e1fdb9f24ec3
Alan Knowles <alan_k@php.net>
2003-03-12 16:27:01 +0000

    oops forgot to add the text element with all the gettext stuff in
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@120009 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Flexy/Token/Text.php
@@ -0,0 +1,220 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2002 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.02 of the PHP license,      |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors:  Alan Knowles <alan@akbkhome>                               |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+//
+ 
+/**
+* Class to Text - implements gettext support.
+* 
+*
+*/
+$GLOBALS['_HTML_TEMPLATE_FLEXY_TOKEN_TEXT']['clean'] = array(
+    '$' => '\$',
+    '"' => '\"',
+    "'" => '\\\'',
+    '\\' => '\\\\',
+    "\n" => '\n',
+    "\t" => '\t',
+    "\r" => '\r'
+);
+$GLOBALS['_HTML_TEMPLATE_FLEXY_TOKEN_TEXT']['unclean'] = array_flip($GLOBALS['_HTML_TEMPLATE_FLEXY_TOKEN_TEXT']['clean']);
+
+class HTML_Template_Flexy_Token_Text extends HTML_Template_Flexy_Token {
+    /**
+    * toString - generate PHP code 
+    * @see parent::toString(), $this->pullState()
+    */
+    
+    
+    function toString() {
+        // if it's XML then quote it..
+        
+        
+        if (!strlen(trim($this->value) )) {
+            return $this->value;
+        }
+        if (!count($this->argTokens) && !$this->isWord()) {
+            return $this->value;
+        }
+        $front = '';
+        $rear = '';
+        for ($i=0;$i<strlen($i); $i++) {
+            if (strpos(" \n\t\r\0\x0B", $this->value{$i}) !== false) {
+                $front .= $this->value{$i};
+                continue;
+            }
+            break;
+        }
+         
+        for ($i=strlen($i);$i>-1; $i--) {
+            if (strpos(" \n\t\r\0\x0B", $this->value{$i}) !== false) {
+                $rear = $this->value{$i} . $rear;
+                continue;
+            }
+            break;            
+        }
+        
+        $value = trim($this->value);
+        
+        
+        
+        $value = strtr($value,$GLOBALS['_HTML_TEMPLATE_FLEXY_TOKEN_TEXT']['clean']);
+        
+        if (!count($this->argTokens)) {
+           
+            
+            $GLOBALS['_HTML_TEMPLATE_FLEXY_TOKEN']['gettextStrings'][] = $value;
+            $value = gettext($value);
+            $value = strtr($value,$GLOBALS['_HTML_TEMPLATE_FLEXY_TOKEN_TEXT']['unclean']);
+        
+            return $front .  $value  . $rear;
+        }
+        //print_r($this->argTokens );
+        $args = array();
+        $argsMake = '';
+        foreach($this->argTokens as $i=>$token) {
+            
+            $args[] =$token->toString();
+            
+        }
+        $GLOBALS['_HTML_TEMPLATE_FLEXY_TOKEN']['gettextStrings'][] = $value;
+        $value = gettext($value);
+        $value = strtr($value,$GLOBALS['_HTML_TEMPLATE_FLEXY_TOKEN_TEXT']['unclean']);
+        
+        
+        
+        $bits = explode('%s',$value);
+        $ret = $front;
+        foreach($bits as $i=>$v) {
+            $ret.=$v.@$args[$i];
+        }
+        
+        return  $ret . $rear;
+        
+        
+        
+        
+    }
+        
+    /**
+    * List of argument tokens.
+    *
+    * @var array
+    * @access public
+    */
+    var $argTokens = array();
+    
+    /**
+    * Search backwards for whitespace and flexy tags to add to string.
+    *
+    * @return   none
+    * @access   public
+    */
+  
+    function backSearch() {
+        // if this is an empty string ignore it?
+        if (!strlen(trim($this->value))) {
+            return;
+        }
+        global $_HTML_TEMPLATE_FLEXY_TOKEN;
+        $i = $this->id -1;
+        while ($i > 0) {
+        
+            $token = $_HTML_TEMPLATE_FLEXY_TOKEN['tokens'][$i];
+            
+            switch (get_class($token)) {
+                case 'html_template_flexy_token_text';
+                    $this->value = $token->value . $this->value;
+                    
+                    unset($_HTML_TEMPLATE_FLEXY_TOKEN['tokens'][$i]);
+                    break;
+                    
+                //case 'html_template_flexy_token_method';
+                case 'html_template_flexy_token_var';
+                    $this->value = '%s'. $this->value;
+                    array_unshift($this->argTokens,$token);
+                    unset($_HTML_TEMPLATE_FLEXY_TOKEN['tokens'][$i]);
+                    break;
+                
+               
+                default:
+                
+                    // found a stop point.
+                    
+                
+                
+                    return;
+            }
+            $i--;
+        }
+    
+    }
+    /**
+    * Search forwards for whitespace and flexy tags to add to string.
+    *
+    * @param   int - id of last tag
+    * @return   int - id of next tag.
+    * @access   public
+    */
+    
+    function forwardSearch($max) {
+       
+        global $_HTML_TEMPLATE_FLEXY_TOKEN;
+        $i = $this->id +1;
+        while ($i < $max) {
+        
+            $token = $_HTML_TEMPLATE_FLEXY_TOKEN['tokens'][$i];
+            
+            switch (get_class($token)) {
+                case 'html_template_flexy_token_text';
+                    $this->value .= $token->value;
+                    unset($_HTML_TEMPLATE_FLEXY_TOKEN['tokens'][$i]);
+                    break;
+                    
+                //case 'html_template_flexy_token_method';
+                case 'html_template_flexy_token_var';
+                    $this->value .= '%s';
+                    $this->argTokens[] = $token;
+                    unset($_HTML_TEMPLATE_FLEXY_TOKEN['tokens'][$i]);
+                    break;
+                default:
+                    return $i - 1;
+            }
+            $i++;
+        }
+        return $i - 1;
+    }
+    
+    function isWord() {
+        if (!strlen(trim($this->value))) {
+            return false;
+        }
+        if (preg_match('/^\&[a-z0-9]+;$/i',trim($this->value))) {
+            return false;
+        }
+        return  preg_match('/[a-z]/i',$this->value);
+    }
+
+}
+
+
+ 
+ 
+   
+?>
