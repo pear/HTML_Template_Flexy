61ca1b0905dc8280a0a41220a085a5241626cc30
Alan Knowles <alan_k@php.net>
2003-07-26 04:55:25 +0000

    add Tag handling to compiler
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@135994 c90b9560-bf6c-de11-be94-00142212c4b1

--- Flexy/Compiler/Standard/Tag.php
+++ Flexy/Compiler/Standard/Tag.php
@@ -7,14 +7,545 @@
 * I guess this class should deal with the main namespace
 * and the parent (standard compiler can redirect other namespaces to other classes.
 *
+* one instance of these exists for each namespace.
 *
 *
 * @version    $Id$
 */
+
 class HTML_Template_Flexy_Compiler_Standard_Tag {
 
+        
+    /**
+    * Parent Compiler for 
+    *
+    * @var  object  HTML_Template_Flexy_Compiler  
+    * 
+    * @access public
+    */
+    var $compiler;
 
+    /**
+    * Factory method to create Tag Handlers
+    *
+    * Long Description
+    * 
+    * 
+    * @param   string    Namespace handler for element.
+    * @param   object   HTML_Template_Flexy_Compiler  
+    * 
+    *
+    * @return    object    tag compiler
+    * @access   public
+    */
+    
+    function &factory($type,&$compiler) {
+        if (!$type) {
+            $type = 'tag';
+        }
+        require_once 'HTML/Template/Flexy/Compiler/Standard/' . ucfirst(strtolower($type)) . '.php';
+        $class = 'HTML_Template_Flexy_Compiler_Standard_' . $type;
+        $ret = new $class;
+        $ret->compiler = &$compiler;
+        return $ret;
+    }
+        
+        
+    /**
+    * The current element to parse..
+    *
+    * @var object
+    * @access public
+    */    
+    var $element;
+    
+    /**
+    * Flag to indicate has attribute flexy:foreach (so you cant mix it with flexy:if!)
+    *
+    * @var boolean
+    * @access public
+    */    
+    var $hasForeach = false;
+    
+    
+    
+    
+    /**
+    * toString - display tag, attributes, postfix and any code in attributes.
+    * Note first thing it does is call any parseTag Method that exists..
+    *
+    * 
+    * @see parent::toString()
+    */
+    function toString($element) 
+    {
+        
+        global $_HTML_TEMPLATE_FLEXY_TOKEN;
+        
+        // store the element in a variable
+        $this->element = $element;
+        //echo "toString: Line {$this->line} <{$this->tag}>\n"; 
+        
+        // if the FLEXYSTARTCHILDREN flag was set, only do children
+        // normally set in BODY tag.
+        // this will probably be superseeded by the Class compiler.
+        
+        if ($element->startChildren) {
+            return $element->compileChildren($this->complier);
+        }
+        
+        $flexyignore = $this->parseAttributeIgnore();
+        
+        // rewriting should be done with a tag.../flag.
+        
+        $this->reWriteURL("HREF");
+        $this->reWriteURL("SRC");
+        
+        // handle elements
+        if (($ret =$this->parseTags()) !== false) {
+            return $ret;
+        }
+        // these add to the close tag..
+        
+        $ret  = $this->parseAttributeForeach();
+        $ret .= $this->parseAttributeIf();
+        
+        // spit ou the tag and attributes.
+        
+        $ret .=  "<". $element->oTag;
+        foreach ($element->attributes as $k=>$v) {
+            // if it's a flexy tag ignore it.
+            
+            if (strtoupper(substr($k,0,6)) == 'FLEXY:') {
+                continue;
+            }
+            // true == an attribute without a ="xxx"
+            if ($v === true) {
+                $ret .= " $k";
+                continue;
+            }
+            
+            // if it's a string just dump it.
+            if (is_string($v)) {
+                $ret .=  " {$k}={$v}";
+                continue;
+            }
+            
+            // normally the value is an array of string, however
+            // if it is an object - then it's a conditional key.
+            // eg.  if (something) echo ' SELECTED';
+            // the object is responsible for adding it's space..
+            
+            if (is_object($v)) {
+                $ret .= $v->compile($this->compiler);
+                continue;
+            }
+            
+            // otherwise its a key="sometext{andsomevars}"
+            
+            $ret .=  " {$k}=";
+            foreach($v as $item) {
+                if (is_string($item)) {
+                    $ret .= $item;
+                    continue;
+                }
+                $ret .= $item->compile($this->compiler);
+            }
+        }
+        $ret .= ">";
+        
+        // post stuff this is probably in the wrong place...
+        
+        if ($element->postfix) {
+            foreach ($element->postfix as $e) {
+                $ret .= $e->compile($this->compiler);
+            }
+        }
+        // output the children.
+        
+        $ret .= $element->compilerChildren($this->compiler);
+        
+        // output the closing tag.
+        
+        if ($element->close) {
+            $ret .= $element->close->compile($this->compiler);
+        }
+        
+        // reset flexyignore
+        
+        $_HTML_TEMPLATE_FLEXY_TOKEN['flexyIgnore'] = $flexyignore;
+        
+        return $ret;
+    }
+    /**
+    * Reads an flexy:foreach attribute - 
+    *
+    *
+    * @return   string to add to output.
+    * @access   public
+    */
+    
+    function parseAttributeIgnore() 
+    {
+    
+        global $_HTML_TEMPLATE_FLEXY_TOKEN;
+        
+        $flexyignore = $_HTML_TEMPLATE_FLEXY_TOKEN['flexyIgnore'];
+        
+        if ($this->element->getAttribute('FLEXY:IGNORE')) {
+            $_HTML_TEMPLATE_FLEXY_TOKEN['flexyIgnore'] = true;
+            $this->element->clearAttribute('FLEXY:IGNORE');
+        }
+        return $flexyignore;
 
+    }
+    
+    /**
+    * Reads an flexy:foreach attribute - 
+    *
+    *
+    * @return   string to add to output.
+    * @access   public
+    */
+    
+    function parseAttributeForeach() 
+    {
+        $foreach = $this->element->getAttribute('FLEXY:FOREACH');
+        if ($foreach === false) {
+            return '';
+        }
+        $this->hasForeach = true;
+        // create a foreach element to wrap this with.
+        $foreachObj =  $this->element->factory('Foreach',
+                explode(',',$foreach),
+                $this->element->line);
+                
+        // does it have a closetag?
+        if (!$this->element->close) {
+            PEAR::raiseError(
+                "An flexy:if attribute was found in &lt;{$this->element->name} tag without a corresponding &lt;/{$this->element->name}
+                    tag on Line {$this->element->line} &lt;{$this->element->tag}&gt;",
+                 null, PEAR_ERROR_DIE);
+        }
+        $this->element->close->postfix = array($this->factory("End", '', $this->element->line));
+
+        $this->element->clearAttribute('FLEXY:FOREACH');
+        return $foreachObj->compile($this->compiler);
+    }
+    /**
+    * Reads an flexy:if attribute - 
+    *
+    *
+    * @return   string to add to output.
+    * @access   public
+    */
+    
+    function parseAttributeIf() 
+    {
+        // dont use the together, if is depreciated..
+        $if = $this->element->getAttribute('FLEXY:IF');
+        
+        if ($if === false) {
+            return '';
+        }
+        
+        if ($this->hasForeach) {
+            PEAR::raiseError(
+                "You may not use FOREACH and IF tags in the same tag on Line {$this->element->line} &lt;{$this->element->tag}&gt;",
+                 null, PEAR_ERROR_DIE);
+        }
+        
+        // allow if="!somevar"
+        $ifnegative = '';
+        
+        if ($if{0} == '!') {
+            $ifnegative = '!';    
+            $if = substr($if,1);
+        }
+        // if="xxxxx"
+        // if="xxxx.xxxx()" - should create a method prefixed with 'if:'
+        // these checks should really be in the if/method class..!!!
+        
+        if (!preg_match('/^[_A-Z][A-Z0-9_]*(\[[0-9]+\])?(\.[_A-Z][A-Z0-9_]*(\[[0-9]+\])?)*(\(\))?$/i',$if)) {
+            PEAR::raiseError(
+                "IF tags only accept simple object.variable or object.method() values on Line {$this->element->line} &lt;{$this->element->tag}&gt;",
+                 null, PEAR_ERROR_DIE);
+        }
+        
+        if (substr($if,-1) == ')') {
+            $ifObj =  $this->element->factory('Method',
+                    array('if:'.$ifnegative.substr($if,0,-2), array()),
+                    $this->element->line);
+        } else {
+            $ifObj =  $this->element->factory('If', $ifnegative.$if, $this->element->line);
+        }
+        
+        // does it have a closetag? - you must have one - so you will have to hack in <span flexy:if=..><img></span> on tags
+        // that do not have close tags - it's done this way to try and avoid mistakes.
+        if (!$this->element->close) {
+            PEAR::raiseError(
+                "An flexy:if attribute was found in &lt;{$this->element->name} tag without a corresponding &lt;/{$this->element->name}
+                    tag on Line {$this->element->line} &lt;{$this->element->tag}&gt;",
+                 null, PEAR_ERROR_DIE);
+        }
+        $this->element->close->postfix = array($this->element->factory("End",'', $this->line));
+        $this->clearAttribute('IF');
+        $this->clearAttribute('FLEXY:IF');
+        return $ifObj->compile($this->compiler);
+    }
+    
+     /**
+    * Reads Tags - and ralays to parseTagXXXXXXX
+    *
+    *
+    * @return   string | false = html output or ignore (just output the tag)
+    * @access   public
+    */
+    
+    
+    function parseTags() 
+    {
+        global $_HTML_TEMPLATE_FLEXY_TOKEN;
+        // doesnt really need strtolower etc. as php functions are not case sensitive!
+        
+        if ($this->element->getAttribute('FLEXY:DYNAMIC')) {
+            return $this->asElement( $this->getAttribute('ID'));
+            
+        }
+            
+        
+        $method = 'parseTag'.$this->tag;
+        if (!$_HTML_TEMPLATE_FLEXY_TOKEN['flexyIgnore'] && method_exists($this,$method)) {
+            return $this->$method();
+            // allow the parse methods to return output.
+        }
+        return false;
+    }
+           
+    /**
+    * produces the code for dynamic elements
+    *
+    * @return   string | false = html output or ignore (just output the tag)
+    * @access   public
+    */
+        
+    function getElementPhp($id) {
+        
+        global $_HTML_TEMPLATE_FLEXY;
+        if (!$id) {
+            
+             PEAR::raiseError("Error:{$_HTML_TEMPLATE_FLEXY['filename']} on Line {$this->element->line} &lt;{$this->element->tag}&gt;: 
+             Dynamic tags require an ID value",
+             null, PEAR_ERROR_DIE);
+        }
+        if (isset($_HTML_TEMPLATE_FLEXY['elements'][$id])) {
+           // echo "<PRE>";print_r($this);print_r($_HTML_TEMPLATE_FLEXY['elements']);echo "</PRE>";
+             PEAR::raiseError("Error:{$_HTML_TEMPLATE_FLEXY['filename']} on Line {$this->element->line} in Tag &lt;{$this->element->tag}&gt;:<BR> 
+             The Dynamic tag Name '$id' has already been used previously by  tag &lt;{$_HTML_TEMPLATE_FLEXY['elements'][$id]->tag}&gt;",
+             null, PEAR_ERROR_DIE);
+        }
+        
+        // this is for a case where you can use a sprintf as the name, and overlay it with a variable element..
+        $_HTML_TEMPLATE_FLEXY['elements'][$id] = $this->toElement($this->element);
+        
+        if ($var = $this->getAttribute('FLEXY:NAMEUSES')) {
+            $var = 'sprintf(\''.$id .'\','.$this->toVar($var) .')';
+            return  
+                'if (!isset($this->elements['.$var.'])) $this->elements['.$var.']= $this->_elements[\''.$id.'\'];
+                $this->elements['.$var.']->attributes[\'name\'] = '.$var. ';
+                echo $this->elements['.$var.']->toHtml();'; 
+        } else {
+           return 'echo $this->_elements[\''.$id.'\']->toHtml();';
+        }
+    }
+    
+    
+    /**
+    * Reads an Input tag - build a element object for it
+    *
+    *
+    * @return   string | false = html output or ignore (just output the tag)
+    * @access   public
+    */
+  
+    function parseTagInput() 
+    {
+        global $_HTML_TEMPLATE_FLEXY;
+        // form elements : format:
+        //value - fill out as PHP CODE
+        
+        // as a general rule, this uses name, rather than ID except on 
+        // radio
+        $id = $this->element->getAttribute('NAME');
+        if (strtoupper($this->element->getAttribute('TYPE')) == 'RADIO') {
+            $id = $this->element->getAttribute('ID');
+            if (!$id) {
+                PEAR::raiseError("Error on Line {$this->element->line} &lt;{$this->element->tag}&gt: 
+                 Radio Input's require an ID tag..",
+                 null, PEAR_ERROR_DIE);
+            }
+            
+        }
+        if (!$id) {
+            return false;
+        }
+        return $this->compiler->appendPhp($this->getElementPhp( $id));
+        
+         
+    
+        
+    }
+    
+    /**
+    * Deal with a TextArea tag - build a element object for it
+    *
+    * @return   string | false = html output or ignore (just output the tag)
+    * @access   public
+    */
+  
+    function parseTagTextArea() 
+    {
+         
+        return $this->compiler->appendPhp(
+            $this->getElementPhp( $this->element->getAttribute('NAME')));
+            
+        
+        
+    }
+    /**
+    * Deal with Selects - build a element object for it (unless flexyignore is set)
+    *
+    *
+    * @return   string | false = html output or ignore (just output the tag)
+    * @access   public
+    */
+  
+    function parseTagSelect() 
+    {
+        return$this->compiler->appendPhp(
+            $this->getElementPhp( $this->element->getAttribute('NAME')));
+    }
+      
+    
+    
+    
+     /**
+    * Reads an Form tag - and set up the element object header etc.
+    *    
+    * @return   string | false = html output or ignore (just output the tag)
+    * @access   public
+    */
+  
+    function parseTagForm() 
+    {
+        global $_HTML_TEMPLATE_FLEXY;
+        $copy = $this->element;
+        $copy->children = array();
+        $id = $this->element->getAttribute('NAME');
+        if (!$id) {
+            $id = 'form';
+        }
+        
+        // this adds the element to the elements array.
+        $old = $this->element;
+        $this->element = $copy;
+        $this->getElementPhp($id);
+        $this->element= $old;
+        
+        
+        return 
+            $this->compiler->appendPhp('echo $this->_elements[\''.$id.'\']->toHtmlnoClose();') .
+            $this->element->compileChildren($this->compiler) .
+            $this->compiler->appendHtml( '</form>');
+    
+    }       
+       
+       
+        
+    
+    
+    /**
+    * reWriteURL - can using the config option 'url_rewrite'
+    *  format "from:to,from:to"
+    * only handle left rewrite. 
+    * so 
+    *  "/images:/myroot/images"
+    * would change
+    *   /images/xyz.gif to /myroot/images/xyz.gif
+    *   /images/stylesheet/imagestyles.css to  /myroot/images/stylesheet/imagestyles.css
+    *   note /imagestyles did not get altered.
+    * will only work on strings (forget about doing /images/{someimage}
+    *
+    *
+    * @param    string attribute to rewrite
+    * @return   none
+    * @access   public
+    */
+    function reWriteURL($which) 
+    {
+        global  $_HTML_TEMPLATE_FLEXY;
+        
+        
+        if (!is_string($original = $this->element->getAttribute($which))) {
+            return;
+        }
+        
+        if ($original == '') {
+            return;
+        }
+        
+        if (empty($_HTML_TEMPLATE_FLEXY['currentOptions']['url_rewrite'])) {
+            return;
+        }
+        
+        $bits = explode(",",$_HTML_TEMPLATE_FLEXY['currentOptions']['url_rewrite']);
+        $new = $original;
+        
+        foreach ($bits as $bit) {
+            $parts = explode (':', $bit);
+            $new = preg_replace('#^'.$parts[0].'#',$parts[1], $new);
+        }
+        
+        
+        if ($original == $new) {
+            return;
+        }
+        $this->element->ucAttributes[$which] = '"'. $new . '"';
+    } 
+    
+    /**
+    * Convert flexy tokens to HTML_Template_Flexy_Elements.
+    *
+    *
+    * @return   array
+    * @access   string
+    */
+    function toElement($element) {
+        require_once 'HTML_Template_Flexy_Element';
+        $ret = new HTML_Template_Flexy_Element;
+        
+        if (get_class($element) != 'html_template_flexy_token_tag') {
+            return $element->value;
+        }
+        
+        
+        $ret->tag = strtolower($element->tag);
+        $ret->attributes = $element->getAttributes();
+        if (!$element->children) {
+            return $ret;
+        }
+        //print_r($this->children);
+        foreach(array_keys($element->children) as $i) {
+            // not quite sure why this happens - but it does.
+            if (!is_object($element->children[$i])) {
+                continue;
+            }
+            $ret->children[] = $this->toElement($element->children[$i]);
+        }
+        return $ret;
+    }
+        
+    
 
 }
 
