7adcc6b14dc28b25832703e6c79f718542ed5172
Alan Knowles <alan_k@php.net>
2003-10-11 02:58:37 +0000

    update examples/test to show script handling ability..
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@142212 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/example.html.en.php
+++ examples/example.html.en.php
@@ -3,8 +3,45 @@
 <head>
 <title>Untitled Document</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+
+
+
 </head>
 
+<script language="javascript">
+
+// some sample javascript that might cause problemss
+
+function CheckDuplicates (AddListContainer, RemoveListContainer) { 
+    var AddList = eval('document.main_form.'+AddListContainer); 
+    var RemoveList = eval('document.main_form.'+RemoveListContainer); 
+    var TempAddList = AddList.value; 
+    var TempRemoveList = RemoveList.value; 
+    if (TempAddList>''&&TempRemoveList>'') { 
+        TempAddList = TempAddList.substring(0,TempAddList.length-1); 
+    }
+}
+ 
+<script>
+<!--
+
+// and now just commented out stuff.. that may cause problems
+
+function CheckDuplicates (AddListContainer, RemoveListContainer) { 
+    var AddList = eval('document.main_form.'+AddListContainer); 
+    var RemoveList = eval('document.main_form.'+RemoveListContainer); 
+    var TempAddList = AddList.value; 
+    var TempRemoveList = RemoveList.value; 
+    if (TempAddList>''&&TempRemoveList>'') { 
+        TempAddList = TempAddList.substring(0,TempAddList.length-1); 
+    }
+}
+  dont use - - two dashes together - it's illegal in HTML :)
+  -> xxx
+  
+
+-->
 <body>
 <p>Example Template for HTML_Template_Flexy</p>
 
