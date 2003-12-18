e2a9e321b0c64109dce98fc8a956aa7f7b5515bb
Alan Knowles <alan_k@php.net>
2003-12-18 02:07:43 +0000

    send out correct javascript type (John Dell)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@146699 c90b9560-bf6c-de11-be94-00142212c4b1

--- Flexy/Compiler/Standard/Flexy.php
+++ Flexy/Compiler/Standard/Flexy.php
@@ -89,7 +89,7 @@ class HTML_Template_Flexy_Compiler_Standard_Flexy  {
     
     function toJavascriptToString($element) {
         $ret = $this->compiler->appendPhp( "require_once 'HTML/Javascript/Convert.php';");
-        $ret .= $this->compiler->appendHTML("\n<script language='javascript'>\n");
+        $ret .= $this->compiler->appendHTML("\n<script language='text/javascript'>\n");
         $prefix = ''. $element->getAttribute('FLEXY:PREFIX');
         
         
@@ -152,4 +152,4 @@ class HTML_Template_Flexy_Compiler_Standard_Flexy  {
 
 }
 
-?>
+ 
