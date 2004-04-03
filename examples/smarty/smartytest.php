7c38b7ac5f917fefa1e69314d65dd5696b2fd1e7
Alan Knowles <alan_k@php.net>
2004-04-03 11:57:37 +0000

    renaming of smarty files to relate more specifically to their tasks.
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@155263 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/smarty/smartytest.php
+++ examples/smarty/smartytest.php
@@ -12,28 +12,21 @@ if (!isset($_SERVER['argv'][1])) {
 //print_r($files);
 foreach($files as $file) {
     $dir = dirname($file);
-    /*
     $x = new HTML_Template_Flexy(array(
                     'compileDir'    =>  dirname(__FILE__) ,      // where do you want to write to..
                     'templateDir'   =>  $dir ,     // where are your templates
                     'locale'        => 'en',    // works with gettext
                     'forceCompile'  =>  true,  // only suggested for debugging
-                                                                                                   
                     'debug'         => false,   // prints a few errors
-                                 
                     'nonHTML'       => false,  // dont parse HTML tags (eg. email templates)
                     'allowPHP'      => false,   // allow PHP in template
-                    'compiler'      => 'SmartyEmulator', // which compiler to use.
-                    'compileToString' => true,    // should the compiler return a string
-                                                                // rather than writing to a file.
+                    'compiler'      => 'SmartyConvertor', // which compiler to use.
+                    'compileToString' => true,    // returns the converted template (rather than actually 
+                                                   // converting to PHP.
                     'filters'       => array(),    // used by regex compiler..
                     'numberFormat'  => ",2,'.',','",  // default number format  = eg. 1,200.00 ( {xxx:n} )
                     'flexyIgnore'   => 0        // turn on/off the tag to element code
                 ));
-   // echo "\n\n:COMPILE $file\n\n";
-   */
-    require_once 'HTML/Template/Flexy/Compiler/SmartyEmulator.php';
-   
-    $x = new HTML_Template_Flexy_Compiler_SmartyEmulator;
-    echo $x->convertToFlexy(file_get_contents($dir.'/'.basename($file)));
+    
+    echo $x->compile(basename($file));
 }
