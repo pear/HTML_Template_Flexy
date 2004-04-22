9949f702b66b7d3f911e9e5d535f029ed6473fa4
Alan Knowles <alan_k@php.net>
2004-04-22 08:34:55 +0000

    add tests for namespaces
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@156768 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/tests/results1/namespaces.html.en.php
@@ -0,0 +1,25 @@
+<?php echo '<?xml version="1.0"?>';?>
+<?php echo '<?xml-stylesheet href="chrome://global/skin/" type="text/css"?>';?>
+ 
+<window id="wndUserResults" title="User Search Results" persist="screenX screenY width height" xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul" xmlns:html="http://www.w3.org/1999/xhtml">
+ 
+        <tree id="userSearchResults" flex="1" height="300" enableColumnDrag="true" ondblclick="parent.parent.userEditPopup();">
+                <treecols>
+                        <treecol flex="2" id="trcName" label="Name" primary="true" persist="width ordinal hidden" />
+                        <splitter class="tree-splitter" />
+                        <treecol flex="1" id="trcGroupName" label="Group Name" persist="width ordinal hidden" />
+                </treecols>
+                <treechildren>
+                        <?php if (is_array($t->sresult)  || is_object($t->sresult)) foreach($t->sresult as $id => $data) {?><treeitem>
+                                <treerow>
+                                        <treecell label="<?php echo htmlspecialchars($data->name);?>" />
+                                        <treecell label="<?php echo htmlspecialchars($data->group_name);?>" />
+                                        <treecell label="<?php echo htmlspecialchars($data->user_id);?>" />
+                                </treerow>
+                        </treeitem><?php }?>
+                </treechildren>
+        </tree>
+        <?php echo $this->elements['test']->toHtmlnoClose();?>
+            <?php echo $this->elements['test2']->toHtml();?>
+        </form>
+</window>
