28e24402dc97295bb3cf0edf335987cfee73ee4f
Alan Knowles <alan_k@php.net>
2004-06-12 05:58:28 +0000

    closer fix introduced yet another bug.. - ignoreing element suffix
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@161122 c90b9560-bf6c-de11-be94-00142212c4b1

--- Flexy/Element.php
+++ Flexy/Element.php
@@ -495,8 +495,9 @@ class HTML_Template_Flexy_Element {
             $tag = $bits[1];
         }
         // tags that never should have closers  
-        $close = in_array(strtoupper($tag),array("INPUT","IMG")) ? '' : "</{$ret->tag}>{$suffix}" ;
-        
+        $close = in_array(strtoupper($tag),array("INPUT","IMG")) ? '' : "</{$ret->tag}>";
+        $close .= $suffix ;
+       
         return "{$prefix}<{$ret->tag}".$ret->attributesToHTML() . '>'.$ret->childrenToHTML() .$close;
         
          
