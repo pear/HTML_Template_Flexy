f3faa829a6abe844b2d923e0be897911411679bd
Alan Knowles <alan_k@php.net>
2009-03-06 08:45:17 +0000

    fix include to only pass variables created by template, rather than flood the subtemplate - fixes various segfaults.
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@276806 c90b9560-bf6c-de11-be94-00142212c4b1

--- Flexy/Compiler/Flexy/Flexy.php
+++ Flexy/Compiler/Flexy/Flexy.php
@@ -234,8 +234,8 @@ class HTML_Template_Flexy_Compiler_Flexy_Flexy  {
                 "\$x = new HTML_Template_Flexy(\$this->options);\n".
                 "\$x->compile({$arg});\n".
                 "\$_t = function_exists('clone') ? clone(\$t) : \$t;\n".
-                "foreach(get_defined_vars()  as \$k=>\$v) {\n" .
-                "    if (\$k != 't') { \$_t->\$k = \$v; }\n" .
+                "foreach(".$element->scopeVarsToArrayString(). "  as \$k) {\n" .
+                "    if (\$k != 't') { \$_t->\$k = \$\$k; }\n" .
                 "}\n" .
                 "\$x->outputObject(\$_t, \$this->elements);\n"
             );
