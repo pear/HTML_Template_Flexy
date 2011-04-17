79bf0940d27904c994b802f71a883274d9144a34
Alan Knowles <alan_k@php.net>
2011-04-17 07:04:18 +0000

    mirror config array, so we can use the common page id from it
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_Flexy/trunk@310274 c90b9560-bf6c-de11-be94-00142212c4b1

--- HTML/Template/Flexy/Compiler/Flexy.php
+++ HTML/Template/Flexy/Compiler/Flexy.php
@@ -236,6 +236,7 @@ class HTML_Template_Flexy_Compiler_Flexy extends HTML_Template_Flexy_Compiler {
     
         if (is_array($this->options['Translation2'])) {
             require_once 'Translation2.php';
+            $this->_options = $this->options; // store the original options..
             $this->options['Translation2'] =  &Translation2::factory(
                 $this->options['Translation2']['driver'],
                 isset($this->options['Translation2']['options']) ? $this->options['Translation2']['options'] : array(),
@@ -246,7 +247,7 @@ class HTML_Template_Flexy_Compiler_Flexy extends HTML_Template_Flexy_Compiler {
         if (is_a($this->options['Translation2'], 'Translation2')) {
             $this->options['Translation2']->setLang($this->options['locale']);
             
-            if(empty($this->options['Translation2']['CommonPageID'])) {
+            if(empty($this->_options['Translation2']['CommonPageID'])) {
 		// fixme - needs to be more specific to which template to use..
     	        foreach ($this->options['templateDir'] as $tt) {
                     $n = basename($this->currentTemplate);
